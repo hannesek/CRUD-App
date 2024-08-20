01c1bd235cab3306c290c5ca57097ab1e95bc94f
hannesek <Hannes.kullberg@gmail.com>
2024-08-20 13:56:08 +0200

    added dockerfile

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa2d1993304c81256feb81cf7f1b06ead436a34a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad4ad1d7fdc1a5f5334c3748dd3e816e355f00c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
